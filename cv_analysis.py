812ea1c5aa7ef187adbbdab311357b73e9288a47
yousifalisoliman-o <yousifalisoliman@gmail.com>
2026-09-01 20:55:15 +0300

    Update cv_analysis.py

--- cv_analysis.py
+++ cv_analysis.py
@@ -2,36 +2,42 @@ from google import genai
 from google.genai import types
 import os
 history=[] # i made this variable so it can give gemini the full history so it dont forget the context
-cv=[] # i made this so i upload all cvs at ones
+# cv=[] # i made this so i upload all cvs at ones
 
-client= genai.Client(api_key="AQ.Ab8RN6IZ-Pzl6IQEpaRQjnd9uRsswS4KjizvFrNeEnbqbOPxLA")# api key
+client= genai.Client(api_key="AQ.Ab8RN6KhjxKUCdp4gEHQ-kdxQrDAMpKNKYFZLXpy453KKc1hFQ")# api key
 folder=r"C:\Users\yousi\Downloads\cv's" # the path for the folder
 
 
-while True: # the loop that will keep it continuing
 
 
-    for file in os.listdir(folder): #os.listdir list everything inside that and  loop over it
+for file in os.listdir(folder): #os.listdir list everything inside that and  loop over it
         path = os.path.join(folder, file) # here where we join the full path for gemini not just name x.pdf
 
-        pdf = client.files.upload(file=path)# here is where we upload it to gemini
-        cv.append(pdf)# we append to cv to make a list and upload it ones to cv not 50 upload
+        pdf = client.files.upload(file=path, config=types.UploadFileConfig(
+            display_name=file))  # here is where we upload it to gemini
+        # cv.append(pdf)# we append to cv to make a list and upload it ones to cv not 50 upload
+        history.append(f"The following resume file is named: {file}")
         history.append(pdf)# adding to history
 
 
     #normal part for  asking  gemini
-    ask=input("what is your question?")
-    history.append(ask)
-    response = client.models.generate_content(
+ask=input("what is your question?")
+history.append(ask)
+with open("questions.txt", "w") as file:
+    file.write(ask)
+response = client.models.generate_content(
         model="gemini-3.5-flash",
-        contents=[ask,history,cv],
+        contents=history,
         config=types.GenerateContentConfig(
             temperature=0,
-            system_instruction="no asterisk + tell me at the bottom how many cv you got and the exact time you took to analyze your question"
+            system_instruction="if i asked you to give me the best fit  then (Choose 3 candidates and give a brief introduction of each. Refer to each candidate by their file's display name, not by job title or position) else if i asked you any other question answer that question and dont give me three candite"
         )
 
     )
-    print(response.text)
-    history.append(response.text)
+print(response.text)
+history.append(response.text)
+
+
+
 
 
